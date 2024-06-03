50841245245901f77d723701b0034ae9b6c614aa
Braden <brado.dabest@hotmail.com>
2024-06-03 21:03:11 +1000

    Updated handling

--- src/main/java/com/braden/ServerApplication.java
+++ src/main/java/com/braden/ServerApplication.java
@@ -1,8 +1,16 @@
 package com.braden;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.braden.controller","com.braden.service" })
@@ -12,4 +20,21 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Component
+	public class CorsFilter extends OncePerRequestFilter {
+
+		@Override
+		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+			response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+			if ("OPTIONS".equals(request.getMethod())) {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} else {
+				filterChain.doFilter(request, response);
+			}
+		}
+	}
 }
